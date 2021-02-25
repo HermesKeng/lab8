acd57d5dd659a684ee9d85c1a6317ff16917d9ae
HermesKeng <kaining1101@gmail.com>
2021-02-25 09:12:29 -0800

    lab submission

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,18 @@
 function initMap() {
-	// add your code here
+  // add your code here
+  L.mapquest.key = "2gsXDvnndg0Ghnj4GTndYwuql0moTosc";
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map("map", {
+    center: [32.8797205, -117.2361952],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 15,
+  });
+
+  L.marker([32.8785203, -117.235886], {
+    icon: L.mapquest.icons.marker(),
+    draggable: false,
+  })
+    .bindPopup("SSC Multipurpose Room")
+    .addTo(map);
 }
